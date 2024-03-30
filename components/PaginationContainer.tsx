a3c3fd2fa4b9b6d5f426fccc5bb5f8172c4dfeda
MomenAbdelwadoud <momenwadoudg@gmail.com>
2024-03-30 22:15:34 +0300

    Load more functionality

--- components/PaginationContainer.tsx
+++ components/PaginationContainer.tsx
@@ -1,16 +1,43 @@
+"use client";
 import {getPokemonCardList} from "@/utils/pokemons.service";
-import {Suspense} from "react";
+import {PokemonCardProps} from "@/utils/types";
+import {useEffect, useState} from "react";
 import CardGrid from "./CardGrid";
+import LoadBtn from "./LoadBtn";
 import SpinnerComponent from "./Spinner";
 
-const PaginationContainer = async () => {
-	const pokemonCardList = await getPokemonCardList(10);
+const PaginationContainer = () => {
+	const POKEMONS_PER_PAGE = 20;
+	const [pokemonCardList, setPokemonCardList] = useState<PokemonCardProps[] | null>(
+		null
+	);
+	const [isPending, setIsPending] = useState<boolean>(false);
+	const [currPage, setCurrPage] = useState(0);
+
+	useEffect(() => {
+		const offset = currPage * POKEMONS_PER_PAGE;
+		getPokemonCardList(POKEMONS_PER_PAGE, currPage).then(res =>
+			setPokemonCardList(res)
+		);
+	}, []);
+
+	useEffect(() => {
+		setIsPending(true);
+		getPokemonCardList(POKEMONS_PER_PAGE, currPage * POKEMONS_PER_PAGE).then(res => {
+			setPokemonCardList(res);
+			setIsPending(false);
+		});
+	}, [currPage]);
 
 	return (
-		<div>
-			<Suspense fallback={<SpinnerComponent></SpinnerComponent>}>
-				<CardGrid pokemonList={pokemonCardList}></CardGrid>
-			</Suspense>
+		<div className="flex flex-col max-h-[95%] flex-1 items-center justify-around gap-2">
+			{pokemonCardList === null || isPending ? (
+				<SpinnerComponent></SpinnerComponent>
+			) : (
+				<CardGrid pokemonList={pokemonCardList!}></CardGrid>
+			)}
+
+			<LoadBtn onClick={() => setCurrPage(currPage + 1)}></LoadBtn>
 		</div>
 	);
 };
